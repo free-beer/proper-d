97396a7f66249d6ea33ce31c5723df3ba6c81e8a
Peter Wood <pwood@blacknorth.com>
2013-12-20 18:37:10 +0000

    Initial check in of the project sources.

--- README.md
+++ README.md
@@ -1,4 +1,74 @@
-proper-d
-========
+# Proper-D
 
-A D language library for reading and interacting with collections of properties represented by an associative array of strings indexed by string.
+Proper-D is a simple library of functions for interacting with property files
+(i.e. files that contain simple name value pairs). This is similar in nature
+to the functionality offered by the java.util.Properties class in Java but with
+a D twist. The library effectively provides a very basic file format for use as
+things like of configuration files.
+
+## Building
+
+The library uses the [dub package manager](http://code.dlang.org/) so you will
+need to install that. To build the unit tests for the library clone the
+repository and, from the root directory of the repository copy, run this
+command...
+
+```
+   dub build --build=unittest
+```
+
+Once this successfully completes there should be an executable created in the
+same directory called proper-d.exe on Windows or proper-d on Linux. Run this
+executable to execute the unit tests.
+
+In terms of using the library the simplist thing to do is to copy the main
+source file (source/proper-d.d) into your project. It has no dependencies beyond
+elements of the D standard library.
+
+## Usage
+
+In Java the Properties class completely wraps the functionality for handling
+property files but D provides associative arrays which would obviate the need
+for a complete class. With this in mind the Proper-D code is worked around
+creating associative arrays of strings indexed by strings. To make use of
+Proper-D include the source file in your project and then import it into the
+code to use it with a line such as...
+
+```
+   import properd;
+```
+
+Next you can either load a file of properties or provide a string that can be
+parsed into an associative array. Some examples follow...
+
+```
+   // Load the ./examples.cfg file as a property file.
+   auto properties = readProperties("./examples.cfg");
+
+   // Same as before but specified via a File object.
+   properties = readProperties(new File("./examples.cfg", "r"));
+
+   // Parse a string of text into a property list.
+   properties = parseProperties("first = One\nsecond = 2\nThird = 3.14");
+```
+
+All of the function calls shown above return associative arrays of type string
+indexed by string (i.e. ```string[string]```). No interpretation of the data
+is made but the library does provide an as() function that will attempt to
+convert the values from an associative array to a specific type. The following
+is an example of using this function...
+
+```
+   auto value1 = properties.as!(int)("second");
+```
+
+Here an attempt is made to convert the value indexed under the key "second" in
+the associative array ```properties``` to an ```int```. If this fails then an
+exception will be raised. A specialization of this template function is
+provided for conversion of values to the bool type. This specialization will
+recognise "true", "1", "on" and "yes" (test is case insensitive) as being a
+boolean value of true. Anything else is false.
+
+## License
+
+Consult the license.txt file for licensing information.
---
+++ source/app.d
@@ -0,0 +1,107 @@
+import std.stdio;
+import properd;
+
+void main() { 
+   return;
+}
+
+unittest {
+   import std.conv, std.exception;
+
+   auto properties = readProperties("./test.properties");
+
+   assert(properties.length == 15);
+   assert("first" in properties);
+   assert("second" in properties);
+   assert("third" in properties);
+   assert("integer_0" in properties);
+   assert("integer_1" in properties);
+   assert("integer_123" in properties);
+   assert("float_3.14" in properties);
+   assert("bool_true" in properties);
+   assert("bool_false" in properties);
+   assert("bool_1" in properties);
+   assert("bool_0" in properties);
+   assert("bool_on" in properties);
+   assert("bool_off" in properties);
+   assert("bool_yes" in properties);
+   assert("bool_no" in properties);
+
+   assert(text(typeid(properties.as!(byte)("integer_123"))) == "byte");
+   assert(text(typeid(properties.as!(ubyte)("integer_123"))) == "ubyte");
+   assert(text(typeid(properties.as!(short)("integer_123"))) == "short");
+   assert(text(typeid(properties.as!(ushort)("integer_123"))) == "ushort");
+   assert(text(typeid(properties.as!(uint)("integer_123"))) == "uint");
+   assert(text(typeid(properties.as!(int)("integer_123"))) == "int");
+   assert(text(typeid(properties.as!(long)("integer_123"))) == "long");
+   assert(text(typeid(properties.as!(float)("float_3.14"))) == "float");
+   assert(text(typeid(properties.as!(double)("float_3.14"))) == "double");
+   assert(text(typeid(properties.as!(real)("float_3.14"))) == "real");
+   assert(text(typeid(properties.as!(bool)("bool_true"))) == "bool");
+
+   assert(properties.as!(byte)("integer_0") == 0);
+   assert(properties.as!(byte)("integer_1") == 1);
+   assert(properties.as!(byte)("integer_123") == 123);
+
+   assert(properties.as!(ubyte)("integer_0") == 0);
+   assert(properties.as!(ubyte)("integer_1") == 1);
+   assert(properties.as!(ubyte)("integer_123") == 123);
+
+   assert(properties.as!(short)("integer_0") == 0);
+   assert(properties.as!(short)("integer_1") == 1);
+   assert(properties.as!(short)("integer_123") == 123);
+
+   assert(properties.as!(ushort)("integer_0") == 0);
+   assert(properties.as!(ushort)("integer_1") == 1);
+   assert(properties.as!(ushort)("integer_123") == 123);
+
+   assert(properties.as!(uint)("integer_0") == 0);
+   assert(properties.as!(uint)("integer_1") == 1);
+   assert(properties.as!(uint)("integer_123") == 123);
+
+   assert(properties.as!(int)("integer_0") == 0);
+   assert(properties.as!(int)("integer_1") == 1);
+   assert(properties.as!(int)("integer_123") == 123);
+
+   assert(properties.as!(long)("integer_0") == 0);
+   assert(properties.as!(long)("integer_1") == 1);
+   assert(properties.as!(long)("integer_123") == 123);
+
+   assert(properties.as!(byte)("non_existent") == byte.init);
+   assert(properties.as!(ubyte)("non_existent") == ubyte.init);
+   assert(properties.as!(short)("non_existent") == short.init);
+   assert(properties.as!(ushort)("non_existent") == ushort.init);
+   assert(properties.as!(uint)("non_existent") == uint.init);
+   assert(properties.as!(int)("non_existent") == int.init);
+   assert(properties.as!(long)("non_existent") == long.init);
+   assert(properties.as!(float)("non_existent") is float.init);
+   assert(properties.as!(double)("non_existent") is double.init);
+   assert(properties.as!(real)("non_existent") is real.init);
+
+   assert(properties.as!(byte)("non_existent", -10) == -10);
+   assert(properties.as!(ubyte)("non_existent", 10) == 10);
+   assert(properties.as!(short)("non_existent", -50) == -50);
+   assert(properties.as!(ushort)("non_existent", 50) == 50);
+   assert(properties.as!(uint)("non_existent", 100) == 100);
+   assert(properties.as!(int)("non_existent", 200) == 200);
+   assert(properties.as!(long)("non_existent", 300) == 300);
+   assert(properties.as!(float)("non_existent", 0.0) == 0.0);
+   assert(properties.as!(double)("non_existent", 0.0) == 0.0);
+   assert(properties.as!(real)("non_existent", 0.0) == 0.0);
+
+   assert(properties.as!(bool)("bool_true") == true);
+   assert(properties.as!(bool)("bool_false") == false);
+   assert(properties.as!(bool)("bool_1") == true);
+   assert(properties.as!(bool)("bool_0") == false);
+   assert(properties.as!(bool)("bool_yes") == true);
+   assert(properties.as!(bool)("bool_no") == false);
+   assert(properties.as!(bool)("bool_on") == true);
+   assert(properties.as!(bool)("bool_off") == false);
+   assert(properties.as!(bool)("non_existent") == false);
+   assert(properties.as!(bool)("non_existent", true) == true);
+
+   assertThrown!(PropertyException)(readProperties("./does_not_exist.txt"));
+   assertThrown!(PropertyException)(properties.as!(uint)("first"));
+
+   writeln("*** UNIT TESTS COMPLETED SUCCESSFULLY.");
+}
